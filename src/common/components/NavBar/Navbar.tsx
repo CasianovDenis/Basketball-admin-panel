3b6c556bce6ba4af53eb7e2c6ac4eb3651d44ead
CasianovDenis <denis.casianov@yahoo.com>
2023-06-18 14:59:18 +0300

    Added Navbar

---
+++ src/common/components/NavBar/Navbar.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import {
+  BoxArrowLeft,
+  PersonCircle,
+  PeopleFill,
+  PersonFill,
+} from "react-bootstrap-icons";
+import "./NavBar.css";
+
+export default function SignIn() {
+  const navigate = useNavigate();
+
+  const [decoded_token, setDecoded_token] = useState<any>(null);
+
+  const [teams_color, setTeamsColor] = useState("currentColor");
+  const [players_color, setPlayersColor] = useState("currentColor");
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/Teams") {
+      setTeamsColor("#d33864");
+      setPlayersColor("currentColor");
+    } else if (location.pathname === "/Players") {
+      setTeamsColor("currentColor");
+      setPlayersColor("#d33864");
+    }
+
+    let token = localStorage.getItem("JWToken");
+    if (token) {
+      var base64 = token.split(".")[1];
+      var token_data = JSON.parse(window.atob(base64));
+      setDecoded_token(token_data);
+    }
+  }, [location.pathname]);
+
+  const exit_account = () => {
+    localStorage.removeItem("JWToken");
+    setDecoded_token(null);
+    navigate("/");
+  };
+  if (decoded_token !== null)
+    return (
+      <>
+        <nav
+          id="sidebarMenu"
+          className="collapse d-lg-block sidebar collapse bg-white"
+        >
+          <div id="navbarSupportedContent">
+            <div className="navbar_user">
+              <label>{decoded_token.user}</label>
+
+              <PersonCircle color={"gray"} size={30} />
+            </div>
+            <hr />
+          </div>
+
+          <div className="list-group list-group-flush mx-3 mt-4">
+            <div
+              onClick={() => navigate("/Teams")}
+              style={{ cursor: "pointer" }}
+            >
+              <PeopleFill
+                color={teams_color}
+                style={{ marginLeft: "15px" }}
+                size={20}
+              />
+              <p style={{ color: teams_color }}>Teams</p>
+            </div>
+            <div
+              onClick={() => navigate("/Players")}
+              style={{ cursor: "pointer" }}
+            >
+              <PersonFill
+                color={players_color}
+                style={{ marginLeft: "15px" }}
+                size={20}
+              />
+              <p style={{ color: players_color }}>Players</p>
+            </div>
+          </div>
+
+          <div className="logout" onClick={exit_account}>
+            <BoxArrowLeft size={22} />
+            <label style={{ marginLeft: "2px" }}>Sign Out</label>
+          </div>
+        </nav>
+
+        <nav
+          id="main-navbar"
+          className="navbar navbar-expand-lg navbar-light bg-white fixed-top"
+        >
+          <div className="container-fluid">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#sidebarMenu "
+              aria-controls="sidebarMenu "
+              aria-expanded="true"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <a className="navbar-brand" href="#">
+              <img src="/Images/logo.png" height="50" />
+            </a>
+
+            <div className="collapse navbar-collapse">
+              <div className="navbar_user">
+                <label>{decoded_token.user}</label>
+
+                <PersonCircle color={"gray"} size={30} />
+              </div>
+            </div>
+          </div>
+        </nav>
+      </>
+    );
+  else return <></>;
+}
