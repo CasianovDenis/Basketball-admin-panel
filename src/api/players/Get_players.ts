1b371efa12f8515cc388860fb89d821ce659e29b
CasianovDenis <denis.casianov@yahoo.com>
2023-06-21 11:18:16 +0300

    Modified api  , added get by team id

--- src/api/players/Get_players.ts
+++ src/api/players/Get_players.ts
@@ -1,15 +1,13 @@
 import Toastify from "toastify-js";
-//import React, { useState } from "react";
-import Get_team from "../teams/Get_team";
 
 export default function Get_players(
   player_name: any,
   page_number: any,
   page_size: any,
+  id_team: any,
   callback: any
 ) {
   const token = localStorage.getItem("JWToken");
-  //const [temp_array, setTempArray] = useState([]);
 
   const requestOptions = {
     method: "GET",
@@ -18,29 +16,57 @@ export default function Get_players(
       Authorization: "Bearer " + token,
     },
   };
+  if (id_team === 0)
+    fetch(
+      "http://dev.trainee.dex-it.ru/api/Player/GetPlayers?Name=" +
+        player_name +
+        "&Page=" +
+        page_number +
+        "&PageSize=" +
+        page_size,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        callback(response);
+      })
+      .catch((ex) => {
+        Toastify({
+          text: "Wrong token ",
+          className: "info",
+          style: {
+            background: "linear-gradient(to right, #eb3349, #f45c43)",
+          },
+        }).showToast();
+        localStorage.removeItem("JWToken");
+        window.location.assign("/");
+      });
+  else
+    fetch(
+      "http://dev.trainee.dex-it.ru/api/Player/GetPlayers?Name=" +
+        player_name +
+        "&Page=" +
+        page_number +
+        "&TeamIds=" +
+        id_team +
+        "&PageSize=" +
+        page_size,
 
-  fetch(
-    "http://dev.trainee.dex-it.ru/api/Player/GetPlayers?Name=" +
-      player_name +
-      "&Page=" +
-      page_number +
-      "&PageSize=" +
-      page_size,
-    requestOptions
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      callback(response);
-    })
-    .catch((ex) => {
-      Toastify({
-        text: "Wrong token ",
-        className: "info",
-        style: {
-          background: "linear-gradient(to right, #eb3349, #f45c43)",
-        },
-      }).showToast();
-      localStorage.removeItem("JWToken");
-      window.location.assign("/");
-    });
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        callback(response);
+      })
+      .catch((ex) => {
+        Toastify({
+          text: "Wrong token ",
+          className: "info",
+          style: {
+            background: "linear-gradient(to right, #eb3349, #f45c43)",
+          },
+        }).showToast();
+        localStorage.removeItem("JWToken");
+        window.location.assign("/");
+      });
 }
