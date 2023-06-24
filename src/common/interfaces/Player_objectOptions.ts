a1f101ee587da6956675983a26f44beaf7aeee44
CasianovDenis <denis.casianov@yahoo.com>
2023-06-24 15:30:15 +0300

    Created interfaces for team and player objects

---
+++ src/common/interfaces/Player_objectOptions.ts
@@ -0,0 +1,11 @@
+export default interface Player_objectOptions {
+  name?: string;
+  number?: number;
+  position?: string;
+  team?: number;
+  birthday?: string;
+  height?: number;
+  weight?: number;
+  avatarUrl?: string;
+  id?: number;
+}
