068fee1d0e5902ff1f66c8c1c0e4d35e2c9bfd7a
CasianovDenis <denis.casianov@yahoo.com>
2023-06-27 00:44:43 +0300

    Added toasters and validation text for all inputs

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, createRef } from "react";
 import { useNavigate } from "react-router-dom";
+import Toastify from "toastify-js";
 
 import style from "./SignIn.module.css";
 
@@ -10,14 +11,15 @@ export default function SignIn() {
   const navigate = useNavigate();
 
   const [message, setMessage] = useState("");
+  const [wrong_password, setWrongPassword] = useState("");
+  const [wrong_login, setWrongLogin] = useState("");
+
   const RefLogin = createRef<HTMLInputElement>(),
     RefPassword = createRef<HTMLInputElement>();
 
   useEffect(() => {
     const token = localStorage.getItem("JWToken");
     if (token !== null) navigate("/Teams");
-    // var base64 = token.split(".")[1];
-    //var decoded_token = JSON.parse(window.atob(base64));
   }, []);
 
   const login = (ev: any) => {
@@ -25,38 +27,62 @@ export default function SignIn() {
     if (RefLogin.current && RefPassword.current) {
       let login = RefLogin.current.value;
       let password = RefPassword.current.value;
-      if (login.length > 0 && password.length > 0) {
-        setMessage("Proccessing");
+      if (login.length > 0) {
+        setWrongLogin("");
 
-        const requestOptions = {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            login: login,
-            password: sha256(password),
-          }),
-        };
+        if (password.length > 0) {
+          setWrongPassword("");
+          Toastify({
+            text: "Proccessing",
+            className: "info",
+            style: {
+              background: "linear-gradient(to right, #fe8c00, #f83600)",
+            },
+          }).showToast();
 
-        fetch("http://dev.trainee.dex-it.ru/api/Auth/SignIn", requestOptions)
-          .then((response) => response.json())
-          .then((jsonContent) => {
-            if (jsonContent.token === undefined)
-              setMessage("Username or password incorrect");
-            else {
-              localStorage.setItem("JWToken", jsonContent.token);
+          const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              login: login,
+              password: sha256(password),
+            }),
+          };
 
-              setMessage("Succesfull authentication");
-              navigate("/Teams");
-            }
-          });
-      } else setMessage("Fields can not be empty");
+          fetch("http://dev.trainee.dex-it.ru/api/Auth/SignIn", requestOptions)
+            .then((response) => response.json())
+            .then((jsonContent) => {
+              if (jsonContent.token === undefined)
+                Toastify({
+                  text: "User with the specified username / password was not found.",
+                  className: "info",
+                  style: {
+                    background: "linear-gradient(to right, #eb3349, #f45c43)",
+                  },
+                }).showToast();
+              else {
+                localStorage.setItem("JWToken", jsonContent.token);
+
+                Toastify({
+                  text: "Succesfull authentication",
+                  className: "info",
+                  style: {
+                    background: "linear-gradient(to right, #1d976c, #93f9b9)",
+                  },
+                }).showToast();
+
+                navigate("/Teams");
+              }
+            });
+        } else setWrongPassword("Field can not be empty");
+      } else setWrongLogin("Field can not be empty");
     }
   };
 
   return (
     <div className={style.container_login}>
       <div className={style.image_container}>
-        <img src={Signin_image}  />
+        <img src={Signin_image} />
       </div>
 
       <div className={style.login_input}>
@@ -64,26 +90,27 @@ export default function SignIn() {
           <p className={style.page_title}>Sign In</p>
 
           <div className={style.inputBox}>
-            <p>Login</p>
+            <label>Login</label>
             <input type="text" ref={RefLogin} />
+            <p style={{ color: "red" }}>{wrong_login}</p>
           </div>
 
           <div className={style.inputBox}>
-            <p>Password</p>
+            <label>Password</label>
             <input type="password" ref={RefPassword} required />
+            <p style={{ color: "red" }}>{wrong_password}</p>
           </div>
 
           <button id={style.button_login} onClick={login}>
             Sign In
           </button>
         </form>
-        <p>
+        <p style={{ marginLeft: "15px", marginTop: "15px" }}>
           Not a member yet?
-          <a href="/SignUp" style={{ marginLeft: "5px", color: "#d33864" }}>
+          <a href="/SignUp" style={{ marginLeft: "5px", color: "#E4163A" }}>
             Sign Up
           </a>
         </p>
-        <p>{message}</p>
       </div>
     </div>
   );
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect, createRef } from "react";
 import { sha256 } from "js-sha256";
 import { useNavigate } from "react-router-dom";
+import Toastify from "toastify-js";
 
 import style from "./SignUp.module.css";
 import Signup_image from "./Signup_image.png";
 export default function SignUp() {
   const navigate = useNavigate();
 
+  const [wrong_name, setWrongName] = useState("");
+  const [wrong_login, setWrongLogin] = useState("");
+  const [wrong_password, setWrongPassword] = useState("");
+  const [wrong_verifie_password, setWrongVerifiePassword] = useState("");
   const [message, setMessage] = useState("");
+
   const [accept_agreement, setAcceptAgreement] = useState(false);
 
   const refName = createRef<HTMLInputElement>(),
@@ -32,56 +38,86 @@ export default function SignUp() {
       let password = refPassword.current.value;
       let login = refLogin.current.value;
       let verification_password = refVerificationPassword.current.value;
-      if (
-        name.length > 0 &&
-        login.length > 0 &&
-        password.length > 0 &&
-        verification_password.length > 0
-      ) {
-        if (accept_agreement === true) {
-          if (password === verification_password) {
-            setMessage("Proccessing");
+      if (name.length > 0) {
+        setWrongName("");
+        if (login.length > 0) {
+          setWrongLogin("");
+          if (password.length > 0) {
+            setWrongPassword("");
+            if (verification_password.length > 0) {
+              setWrongVerifiePassword("");
+              if (accept_agreement === true) {
+                setMessage("");
+                if (password === verification_password) {
+                  Toastify({
+                    text: "Proccessing",
+                    className: "info",
+                    style: {
+                      background: "linear-gradient(to right, #fe8c00, #f83600)",
+                    },
+                  }).showToast();
 
-            const requestOptions = {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({
-                userName: name,
-                login: login,
-                password: sha256(password),
-              }),
-            };
+                  setWrongPassword("");
+                  setWrongVerifiePassword("");
+                  const requestOptions = {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                      userName: name,
+                      login: login,
+                      password: sha256(password),
+                    }),
+                  };
 
-            fetch(
-              "http://dev.trainee.dex-it.ru/api/Auth/SignUp",
-              requestOptions
-            )
-              .then((response) => response.json())
-              .then((jsonContent) => {
-                if (
-                  refLogin.current &&
-                  refPassword.current &&
-                  refName.current &&
-                  refVerificationPassword.current
-                ) {
-                  refLogin.current.value = "";
-                  refPassword.current.value = "";
-                  refName.current.value = "";
-                  refVerificationPassword.current.value = "";
+                  fetch(
+                    "http://dev.trainee.dex-it.ru/api/Auth/SignUp",
+                    requestOptions
+                  )
+                    .then((response) => response.json())
+                    .then((jsonContent) => {
+                      if (
+                        refLogin.current &&
+                        refPassword.current &&
+                        refName.current &&
+                        refVerificationPassword.current
+                      ) {
+                        refLogin.current.value = "";
+                        refPassword.current.value = "";
+                        refName.current.value = "";
+                        refVerificationPassword.current.value = "";
+                      }
+                      Toastify({
+                        text: "Successfully created account",
+                        className: "info",
+                        style: {
+                          background:
+                            "linear-gradient(to right, #1d976c, #93f9b9)",
+                        },
+                      }).showToast();
+                    })
+                    .catch((ex) => {
+                      Toastify({
+                        text: "User already exist",
+                        className: "info",
+                        style: {
+                          background:
+                            "linear-gradient(to right, #eb3349, #f45c43)",
+                        },
+                      }).showToast();
+                    });
+                } else {
+                  setWrongPassword("Passwords don't match");
+                  setWrongVerifiePassword("Passwords don't match");
                 }
-                setMessage("Successfully created account");
-              })
-              .catch((ex) => {
-                setMessage("User already exist");
-              });
-          } else setMessage("Passwords don't match");
-        } else setMessage("Please accept agreements");
-      } else setMessage("Fields can not be empty");
+              } else setMessage("Please accept agreements");
+            } else setWrongVerifiePassword("Field can not be empty");
+          } else setWrongPassword("Field can not be empty");
+        } else setWrongLogin("Field can not be empty");
+      } else setWrongName("Field can not be empty");
     }
   };
 
   const accept = () => {
-
     accept_agreement === false
       ? setAcceptAgreement(true)
       : setAcceptAgreement(false);
@@ -97,33 +133,39 @@ export default function SignUp() {
           <p className={style.page_title}>Sign Up</p>
 
           <div className={style.inputBox}>
-            <label>Name</label>
+            <label style={{ width: "100px" }}>Name </label>
             <input type="text" required ref={refName} />
+            <p style={{ color: "red" }}>{wrong_name}</p>
           </div>
 
           <div className={style.inputBox}>
-            <label>Login</label>
+            <label style={{ width: "100px" }}>Login </label>
             <input type="text" required ref={refLogin} />
+            <p style={{ color: "red" }}>{wrong_login}</p>
           </div>
 
           <div className={style.inputBox}>
             <label>Password</label>
             <input type="password" required ref={refPassword} />
+            <p style={{ color: "red" }}>{wrong_password}</p>
           </div>
 
           <div className={style.inputBox}>
             <label>Enter password again</label>
             <input type="password" required ref={refVerificationPassword} />
+            <p style={{ color: "red" }}>{wrong_verifie_password}</p>
           </div>
-          <input type="checkbox" onClick={accept} />
-          <label style={{ marginLeft: "5px" }}>I accept the agreement</label>
+          <div className={style.agreeement_box}>
+            <input type="checkbox" onClick={accept} />
+            <label style={{ marginLeft: "5px" }}>I accept the agreement</label>
+          </div>
           <button id={style.button_signup} onClick={create_account}>
             Sign Up
           </button>
         </form>
-        <p>
+        <p style={{ marginLeft: "15px", marginTop: "15px" }}>
           Already a member?
-          <a href="/" style={{ marginLeft: "5px", color: "#d33864" }}>
+          <a href="/" style={{ marginLeft: "5px", color: "#E4163A" }}>
             Sign In
           </a>
         </p>
