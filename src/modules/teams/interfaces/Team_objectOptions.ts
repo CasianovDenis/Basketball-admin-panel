a5b98d4daf4db01d1efa9a36b554686e551213d6
CasianovDenis <denis.casianov@yahoo.com>
2023-06-28 01:37:30 +0300

    Changed location

---
+++ src/modules/teams/interfaces/Team_objectOptions.ts
@@ -0,0 +1,8 @@
+export default interface Team_objectOptions {
+  team_name: string;
+  team_yearfoundation: number;
+  team_division: string;
+  team_conference: string;
+  image_url: string;
+  id?: number;
+}
